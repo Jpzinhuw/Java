8d826b92fd2c284fdbd89d4b228a011c27499974
jpzinhuw <joaopedromm.construtiva@outlook.com>
2025-03-19 19:19:29 -0300

    Primeiro commit - teste na aula de Java

---
+++ src/Conta.java
@@ -0,0 +1,15 @@
+public class Conta {
+    private int numConta;
+    private String titular;
+    private double saldo;
+
+    public Conta(int numConta, String titular, double saldo) {
+        this.numConta = numConta;
+        this.titular = titular;
+        this.saldo = 0f;
+    }
+
+
+
+}
+
